3e4b97c1e424878f28744ef4b7499afe159fc2b1
Jeremy Bessard <jeremyb@fastmail.com>
2022-07-17 22:15:02 +0200

    first

---
+++ ExtensionAttributes/uptime.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+days=$(uptime | awk '{ print $4 }' | sed 's/,//g')
+num=$(uptime | awk '{ print $3 }')
+
+# now check how long they've been awake
+if [ "$days" = "days" ]; then
+    echo "<result>$num</result>"
+elif [ "$days" = "day" ]; then
+	echo "<result>1</result>"
+else
+	echo "<result>0</result>"
+fi
